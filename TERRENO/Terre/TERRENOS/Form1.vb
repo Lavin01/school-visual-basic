f068cf50b73ff690664f5b0cbe4df5d7d1bf6e4c
Angel Lavin <bywarepro@gmail.com>
2022-02-21 10:48:03 -0600

    Terreno subido

---
+++ TERRENO/Terre/TERRENOS/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Function CalcularArea() As String
+        If tbxAltura.Text IsNot "" And tbxArea.Text IsNot "" Then
+            Dim resultado As Double = Val(tbxAltura.Text) * Val(tbxBase.Text) / 2
+            Dim resultadoText As String = resultado.ToString + "m2"
+            Return resultadoText
+        End If
+        Return "Faltan datos"
+    End Function
+
+
+    Private Sub TextBox2_TextChanged(sender As System.Object, e As System.EventArgs) Handles tbxBase.TextChanged
+        tbxArea.Text = CalcularArea()
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As System.Object, e As System.EventArgs) Handles tbxAltura.TextChanged
+        tbxArea.Text = CalcularArea()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
