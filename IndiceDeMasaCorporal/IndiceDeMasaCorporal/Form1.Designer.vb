03a720b1869ceac81fa645be3ae43ffa4baf53ac
Angel Lavin <bywarepro@gmail.com>
2022-02-23 10:32:53 -0600

    Ahora si se agrego el IMC para calcular

---
+++ IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbxPeso = New System.Windows.Forms.TextBox()
+        Me.tbxAltura = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbxResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(254, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NOMBRE"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(168, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(235, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(62, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "PESO"
+        '
+        'tbxPeso
+        '
+        Me.tbxPeso.Location = New System.Drawing.Point(32, 112)
+        Me.tbxPeso.Name = "tbxPeso"
+        Me.tbxPeso.Size = New System.Drawing.Size(100, 20)
+        Me.tbxPeso.TabIndex = 3
+        '
+        'tbxAltura
+        '
+        Me.tbxAltura.Location = New System.Drawing.Point(186, 112)
+        Me.tbxAltura.Name = "tbxAltura"
+        Me.tbxAltura.Size = New System.Drawing.Size(100, 20)
+        Me.tbxAltura.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(207, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "ALTURA"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(453, 96)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "IMC"
+        '
+        'tbxResultado
+        '
+        Me.tbxResultado.Location = New System.Drawing.Point(415, 112)
+        Me.tbxResultado.Name = "tbxResultado"
+        Me.tbxResultado.ReadOnly = True
+        Me.tbxResultado.Size = New System.Drawing.Size(100, 20)
+        Me.tbxResultado.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 164)
+        Me.Controls.Add(Me.tbxResultado)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.tbxAltura)
+        Me.Controls.Add(Me.tbxPeso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Indice de Masa Corporal"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tbxPeso As System.Windows.Forms.TextBox
+    Friend WithEvents tbxAltura As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents tbxResultado As System.Windows.Forms.TextBox
+
+End Class
---
+++ IndiceDeMasaCorporal/IndiceDeMasaCorporal/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Function CalcularIMC() As String
+        If tbxAltura.Text IsNot "" And tbxPeso.Text IsNot "" Then
+            Dim resultado As Double = (Val(tbxAltura.Text) * Val(tbxAltura.Text)) / Val(tbxPeso.Text)
+            Dim resultadoText As String = "IMC: " + resultado.ToString
+            Return resultadoText
+        End If
+        Return "Faltan datos"
+    End Function
+
+    Private Sub tbxRender1(sender As System.Object, e As System.EventArgs) Handles tbxAltura.TextChanged
+        tbxResultado.Text = CalcularIMC()
+    End Sub
+
+    Private Sub tbxRender2(sender As System.Object, e As System.EventArgs) Handles tbxPeso.TextChanged
+        tbxResultado.Text = CalcularIMC()
+    End Sub
+End Class
