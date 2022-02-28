fe58bb674d0e53f09181bb5228f63ad7ab8fe09c
Angel Lavin <bywarepro@gmail.com>
2022-02-28 10:48:55 -0600

    Promedio clase diplomado

---
+++ CalcularPromedioDiplomado/CalcularPromedio/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbxM1 = New System.Windows.Forms.Label()
+        Me.tbxM2 = New System.Windows.Forms.Label()
+        Me.tbxM3 = New System.Windows.Forms.Label()
+        Me.tbxM4 = New System.Windows.Forms.Label()
+        Me.tbxM5 = New System.Windows.Forms.Label()
+        Me.tBM1 = New System.Windows.Forms.TextBox()
+        Me.tBM2 = New System.Windows.Forms.TextBox()
+        Me.tBM3 = New System.Windows.Forms.TextBox()
+        Me.tBM4 = New System.Windows.Forms.TextBox()
+        Me.tBM5 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBoxResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre del Alumno"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(381, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre del Diplomado"
+        '
+        'tbxM1
+        '
+        Me.tbxM1.AutoSize = True
+        Me.tbxM1.Location = New System.Drawing.Point(25, 64)
+        Me.tbxM1.Name = "tbxM1"
+        Me.tbxM1.Size = New System.Drawing.Size(51, 13)
+        Me.tbxM1.TabIndex = 2
+        Me.tbxM1.Text = "Materia 1"
+        '
+        'tbxM2
+        '
+        Me.tbxM2.AutoSize = True
+        Me.tbxM2.Location = New System.Drawing.Point(25, 103)
+        Me.tbxM2.Name = "tbxM2"
+        Me.tbxM2.Size = New System.Drawing.Size(51, 13)
+        Me.tbxM2.TabIndex = 3
+        Me.tbxM2.Text = "Materia 2"
+        '
+        'tbxM3
+        '
+        Me.tbxM3.AutoSize = True
+        Me.tbxM3.Location = New System.Drawing.Point(25, 144)
+        Me.tbxM3.Name = "tbxM3"
+        Me.tbxM3.Size = New System.Drawing.Size(51, 13)
+        Me.tbxM3.TabIndex = 4
+        Me.tbxM3.Text = "Materia 3"
+        '
+        'tbxM4
+        '
+        Me.tbxM4.AutoSize = True
+        Me.tbxM4.Location = New System.Drawing.Point(25, 186)
+        Me.tbxM4.Name = "tbxM4"
+        Me.tbxM4.Size = New System.Drawing.Size(51, 13)
+        Me.tbxM4.TabIndex = 5
+        Me.tbxM4.Text = "Materia 4"
+        '
+        'tbxM5
+        '
+        Me.tbxM5.AutoSize = True
+        Me.tbxM5.Location = New System.Drawing.Point(25, 221)
+        Me.tbxM5.Name = "tbxM5"
+        Me.tbxM5.Size = New System.Drawing.Size(51, 13)
+        Me.tbxM5.TabIndex = 6
+        Me.tbxM5.Text = "Materia 5"
+        '
+        'tBM1
+        '
+        Me.tBM1.Location = New System.Drawing.Point(102, 64)
+        Me.tBM1.Name = "tBM1"
+        Me.tBM1.Size = New System.Drawing.Size(100, 20)
+        Me.tBM1.TabIndex = 7
+        '
+        'tBM2
+        '
+        Me.tBM2.Location = New System.Drawing.Point(102, 103)
+        Me.tBM2.Name = "tBM2"
+        Me.tBM2.Size = New System.Drawing.Size(100, 20)
+        Me.tBM2.TabIndex = 8
+        '
+        'tBM3
+        '
+        Me.tBM3.Location = New System.Drawing.Point(102, 141)
+        Me.tBM3.Name = "tBM3"
+        Me.tBM3.Size = New System.Drawing.Size(100, 20)
+        Me.tBM3.TabIndex = 9
+        '
+        'tBM4
+        '
+        Me.tBM4.Location = New System.Drawing.Point(102, 183)
+        Me.tBM4.Name = "tBM4"
+        Me.tBM4.Size = New System.Drawing.Size(100, 20)
+        Me.tBM4.TabIndex = 10
+        '
+        'tBM5
+        '
+        Me.tBM5.Location = New System.Drawing.Point(102, 221)
+        Me.tBM5.Name = "tBM5"
+        Me.tBM5.Size = New System.Drawing.Size(100, 20)
+        Me.tBM5.TabIndex = 11
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(130, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox1.TabIndex = 12
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(501, 21)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(182, 20)
+        Me.TextBox2.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(443, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(151, 29)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "PROMEDIO"
+        '
+        'txtBoxResultado
+        '
+        Me.txtBoxResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxResultado.Location = New System.Drawing.Point(462, 145)
+        Me.txtBoxResultado.Name = "txtBoxResultado"
+        Me.txtBoxResultado.ReadOnly = True
+        Me.txtBoxResultado.Size = New System.Drawing.Size(100, 35)
+        Me.txtBoxResultado.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(721, 261)
+        Me.Controls.Add(Me.txtBoxResultado)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.tBM5)
+        Me.Controls.Add(Me.tBM4)
+        Me.Controls.Add(Me.tBM3)
+        Me.Controls.Add(Me.tBM2)
+        Me.Controls.Add(Me.tBM1)
+        Me.Controls.Add(Me.tbxM5)
+        Me.Controls.Add(Me.tbxM4)
+        Me.Controls.Add(Me.tbxM3)
+        Me.Controls.Add(Me.tbxM2)
+        Me.Controls.Add(Me.tbxM1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tbxM1 As System.Windows.Forms.Label
+    Friend WithEvents tbxM2 As System.Windows.Forms.Label
+    Friend WithEvents tbxM3 As System.Windows.Forms.Label
+    Friend WithEvents tbxM4 As System.Windows.Forms.Label
+    Friend WithEvents tbxM5 As System.Windows.Forms.Label
+    Friend WithEvents tBM1 As System.Windows.Forms.TextBox
+    Friend WithEvents tBM2 As System.Windows.Forms.TextBox
+    Friend WithEvents tBM3 As System.Windows.Forms.TextBox
+    Friend WithEvents tBM4 As System.Windows.Forms.TextBox
+    Friend WithEvents tBM5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtBoxResultado As System.Windows.Forms.TextBox
+
+End Class
---
+++ CalcularPromedioDiplomado/CalcularPromedio/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Function CalcularPromedio() As String
+        If tBM1.Text IsNot "" And tBM2.Text IsNot "" And tBM3.Text IsNot "" And tBM4.Text IsNot "" And tBM5.Text IsNot "" Then
+            Dim resultado As Double = Val(Val(tBM1.Text) + Val(tBM2.Text) + Val(tBM3.Text) + Val(tBM4.Text) + Val(tBM5.Text)) / 5
+            Return resultado
+        End If
+        Return "Faltan datos"
+
+    End Function
+
+    Private Sub txtBox_Materia_1_TextChanged(sender As Object, e As EventArgs) Handles tBM1.TextChanged
+        txtBoxResultado.Text = CalcularPromedio()
+    End Sub
+    Private Sub txtBox_Materia_2_TextChanged(sender As Object, e As EventArgs) Handles tBM2.TextChanged
+        txtBoxResultado.Text = CalcularPromedio()
+    End Sub
+    Private Sub txtBox_Materia_3_TextChanged(sender As Object, e As EventArgs) Handles tBM3.TextChanged
+        txtBoxResultado.Text = CalcularPromedio()
+    End Sub
+    Private Sub txtBox_Materia_4_TextChanged(sender As Object, e As EventArgs) Handles tBM4.TextChanged
+        txtBoxResultado.Text = CalcularPromedio()
+    End Sub
+    Private Sub txtBox_Materia_5_TextChanged(sender As Object, e As EventArgs) Handles tBM5.TextChanged
+        txtBoxResultado.Text = CalcularPromedio()
+    End Sub
+End Class
