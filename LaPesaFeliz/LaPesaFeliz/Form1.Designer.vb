722f901e14a188089871df2002200a26b472da11
Angel Lavin <bywarepro@gmail.com>
2022-03-17 10:25:13 -0600

    Algunos cambios en la logistica de la pesa feliz

---
+++ LaPesaFeliz/LaPesaFeliz/Form1.Designer.vb
@@ -0,0 +1,431 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.radInicial = New System.Windows.Forms.RadioButton()
+        Me.radMedio = New System.Windows.Forms.RadioButton()
+        Me.radAvanzado = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.checkedListBox = New System.Windows.Forms.CheckedListBox()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lstTicket = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtNoC = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtMail = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.LaPesaFeliz.My.Resources.Resources.kisspng_computer_icons_user_clip_art_user_5abf13db5624e4_1771742215224718993529
+        Me.PictureBox3.Location = New System.Drawing.Point(614, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(128, 132)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = Global.LaPesaFeliz.My.Resources.Resources.pngkey_com_dumbbell_png_336464
+        Me.PictureBox2.Location = New System.Drawing.Point(22, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(148, 132)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.LaPesaFeliz.My.Resources.Resources.simply_gym
+        Me.PictureBox1.Location = New System.Drawing.Point(-72, -359)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(935, 533)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 206)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cliente No."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 244)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 322)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Teléfono"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 284)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Direccion"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 361)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "E-Mail"
+        '
+        'radInicial
+        '
+        Me.radInicial.AutoSize = True
+        Me.radInicial.Location = New System.Drawing.Point(10, 39)
+        Me.radInicial.Name = "radInicial"
+        Me.radInicial.Size = New System.Drawing.Size(136, 17)
+        Me.radInicial.TabIndex = 6
+        Me.radInicial.TabStop = True
+        Me.radInicial.Text = "Inicial . . . . . . . $200.00"
+        Me.radInicial.UseVisualStyleBackColor = True
+        '
+        'radMedio
+        '
+        Me.radMedio.AutoSize = True
+        Me.radMedio.Location = New System.Drawing.Point(8, 71)
+        Me.radMedio.Name = "radMedio"
+        Me.radMedio.Size = New System.Drawing.Size(138, 17)
+        Me.radMedio.TabIndex = 7
+        Me.radMedio.TabStop = True
+        Me.radMedio.Text = "Medio . . . . . . . $400.00"
+        Me.radMedio.UseVisualStyleBackColor = True
+        '
+        'radAvanzado
+        '
+        Me.radAvanzado.AutoSize = True
+        Me.radAvanzado.Location = New System.Drawing.Point(10, 108)
+        Me.radAvanzado.Name = "radAvanzado"
+        Me.radAvanzado.Size = New System.Drawing.Size(139, 17)
+        Me.radAvanzado.TabIndex = 8
+        Me.radAvanzado.TabStop = True
+        Me.radAvanzado.Text = "Avanzado . . . . $600.00"
+        Me.radAvanzado.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(17, 108)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton4.TabIndex = 11
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "8:00 PM"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(17, 74)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(71, 17)
+        Me.RadioButton5.TabIndex = 10
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "12:00 PM"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(17, 39)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton6.TabIndex = 9
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "6:00 AM"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radAvanzado)
+        Me.GroupBox1.Controls.Add(Me.radMedio)
+        Me.GroupBox1.Controls.Add(Me.radInicial)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 400)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(166, 153)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PLANES"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton6)
+        Me.GroupBox2.Location = New System.Drawing.Point(191, 400)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(115, 153)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "HORARIOS"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.checkedListBox)
+        Me.GroupBox3.Location = New System.Drawing.Point(331, 400)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(212, 104)
+        Me.GroupBox3.TabIndex = 17
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "SERVICIOS"
+        '
+        'checkedListBox
+        '
+        Me.checkedListBox.FormattingEnabled = True
+        Me.checkedListBox.Items.AddRange(New Object() {"Control de dieta ($50)", "Regaderas ($25)", "Asesoria en Linea ($30)", "Transporte ($20)", "Lockers ($10)"})
+        Me.checkedListBox.Location = New System.Drawing.Point(6, 19)
+        Me.checkedListBox.Name = "checkedListBox"
+        Me.checkedListBox.Size = New System.Drawing.Size(200, 79)
+        Me.checkedListBox.TabIndex = 30
+        '
+        'btnNew
+        '
+        Me.btnNew.Location = New System.Drawing.Point(352, 510)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(105, 43)
+        Me.btnNew.TabIndex = 18
+        Me.btnNew.Text = "Nuevo"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(492, 510)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 43)
+        Me.Button2.TabIndex = 19
+        Me.Button2.Text = "Registro"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(663, 510)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(105, 43)
+        Me.Button3.TabIndex = 20
+        Me.Button3.Text = "SALIR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lstTicket
+        '
+        Me.lstTicket.FormattingEnabled = True
+        Me.lstTicket.Location = New System.Drawing.Point(549, 209)
+        Me.lstTicket.Name = "lstTicket"
+        Me.lstTicket.Size = New System.Drawing.Size(219, 212)
+        Me.lstTicket.TabIndex = 21
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(632, 188)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 13)
+        Me.Label6.TabIndex = 22
+        Me.Label6.Text = "TICKET"
+        '
+        'txtNoC
+        '
+        Me.txtNoC.Location = New System.Drawing.Point(77, 203)
+        Me.txtNoC.Name = "txtNoC"
+        Me.txtNoC.Size = New System.Drawing.Size(117, 20)
+        Me.txtNoC.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(77, 241)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(380, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(77, 281)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(380, 20)
+        Me.txtDireccion.TabIndex = 3
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(77, 319)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(184, 20)
+        Me.txtPhone.TabIndex = 4
+        '
+        'txtMail
+        '
+        Me.txtMail.Location = New System.Drawing.Point(77, 358)
+        Me.txtMail.Name = "txtMail"
+        Me.txtMail.Size = New System.Drawing.Size(380, 20)
+        Me.txtMail.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(620, 439)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 24)
+        Me.Label7.TabIndex = 28
+        Me.Label7.Text = "TOTAL"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(639, 467)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(29, 20)
+        Me.lblTotal.TabIndex = 29
+        Me.lblTotal.Text = "$0"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(331, 83)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 30
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 565)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtMail)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtNoC)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lstTicket)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "LA PESA FELIZ"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents radInicial As System.Windows.Forms.RadioButton
+    Friend WithEvents radMedio As System.Windows.Forms.RadioButton
+    Friend WithEvents radAvanzado As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnNew As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lstTicket As System.Windows.Forms.ListBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtNoC As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtDireccion As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents txtMail As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents checkedListBox As System.Windows.Forms.CheckedListBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+
+End Class
---
+++ LaPesaFeliz/LaPesaFeliz/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+
+    Dim planInicial As Integer = 200
+    Dim planMedio As Integer = 400
+    Dim planAvanzado As Integer = 600
+
+    Dim valorItems As String = ""
+    Function returnValuesUpdated(ByVal elObjeto) As String
+        Dim ValorActual As Double = 0
+        If elObjeto.Checked() = True Then
+            Select elObjeto.Name
+                Case "radInicial"
+                    ValorActual += planInicial
+                Case "radMedio"
+                    ValorActual += planMedio
+                Case "radAvanzado"
+                    ValorActual += planAvanzado
+            End Select
+        End If
+
+        lstTicket.Items.Clear()
+        Dim testArray() As String = Split(valorItems, ",")
+        Dim lastNonEmpty As Integer = -1
+        lstTicket.Items.Add("                     LA PESA FELIZ")
+        lstTicket.Items.Add("                     Tel: 868-000-0000")
+        lstTicket.Items.Add("                     RFC: GPSDCS55CA1DC")
+        lstTicket.Items.Add("------------- CLIENTES REGISTRADOS ---------------------")
+
+        For i As Integer = 0 To testArray.Length - 1
+            If testArray(i) <> "" Then
+                lstTicket.Items.Add(testArray(i))
+            End If
+        Next
+        Return ValorActual
+    End Function
+
+    Private Sub btnNew_Click(sender As System.Object, e As System.EventArgs) Handles btnNew.Click
+        txtNoC.Text = ""
+        txtName.Text = ""
+        txtDireccion.Text = ""
+        txtPhone.Text = ""
+        txtMail.Text = ""
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        lstTicket.Items.Add("                     LA PESA FELIZ")
+        lstTicket.Items.Add("                     Tel: 868-000-0000")
+        lstTicket.Items.Add("                     RFC: GPSDCS55CA1DC")
+        lstTicket.Items.Add("------------- CLIENTES REGISTRADOS ---------------------")
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox(valorItems)
+    End Sub
+
+    Private Sub radInicial_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles radInicial.CheckedChanged
+        lblTotal.Text = returnValuesUpdated(radInicial)
+    End Sub
+
+    Private Sub radMedio_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles radMedio.CheckedChanged
+        lblTotal.Text = returnValuesUpdated(radMedio)
+    End Sub
+
+    Private Sub radAvanzado_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles radAvanzado.CheckedChanged
+        lblTotal.Text = returnValuesUpdated(radAvanzado)
+    End Sub
+
+    Private Sub checkedListBox_ItemCheck(sender As Object, e As System.Windows.Forms.ItemCheckEventArgs) Handles checkedListBox.ItemCheck
+        valorItems = ""
+        Dim i As Integer
+        Dim Valor As String
+        For i = 0 To Me.checkedListBox.CheckedItems.Count - 1
+            Valor = checkedListBox.CheckedItems(i)
+            valorItems += Valor + ","
+        Next
+        returnValuesUpdated(radInicial)
+    End Sub
+End Class
