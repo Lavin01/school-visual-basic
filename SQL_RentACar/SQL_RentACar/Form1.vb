6c55c64a32bcb6b31ea9670f41604f0f79df39d9
Angel Lavin <bywarepro@gmail.com>
2022-04-05 09:52:11 -0600

    Little change in conection

--- SQL_RentACar/SQL_RentACar/Form1.vb
+++ SQL_RentACar/SQL_RentACar/Form1.vb
@@ -6,8 +6,14 @@ Public Class Form1
     Dim cmdCommand As OleDb.OleDbCommand
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Dim con As String = "Provider=Microsoft.ACE.OLED.0.12.0;Data Source=" &
-            Application.StartupPath & "\rentaCoches.accdb"
+        Dim con As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = rentaCoches.accdb"
         conConexion = New OleDb.OleDbConnection(con)
+
+        Try
+            conConexion.Open()
+            MsgBox("Se conecto a la base de datos EXITOSAMENTE")
+        Catch ex As Exception
+            MsgBox("No se logro conectar a la base de datos", vbInformation)
+        End Try
     End Sub
 End Class
